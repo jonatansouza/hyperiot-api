adc0dad02dbb7398263dc22405da573c3057d854
Jonatan <jonatan.gall@ibm.com>
2019-11-21 00:22:30 -0300

    remove unused vars on env

--- config/env.js
+++ config/env.js
@@ -3,11 +3,6 @@
 const ENV = {
     MONGO_URL: 'mongodb://mongo:27017/',
     MONGO_DBNAME: 'hyperiot',
-    CLOUD: {
-        clientId: '',
-        clientSecret: '',
-        redirectUri: ''
-    }
 }
 
 module.exports = ENV;
