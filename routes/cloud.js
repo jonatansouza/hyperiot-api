ca518b823e9661a382b64265fd991f35b2b1d5b6
Jonatan <jonatan.gall@ibm.com>
2019-11-21 00:23:01 -0300

    update google cloud module

--- routes/cloud.js
+++ routes/cloud.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const {google} = require('googleapis');
+const googleStorage = require('../models/google-cloud');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json([]);
+router.get('/', async function(req, res, next) {
+  const files = await googleStorage.getFiles('hyperiot-storage1');
+  res.json(files[0].map(el => el.name));
 });
 
+router.get('/:filename', async function(req, res, next) {
+  const filename = req.params.filename;
+
+  const file = await googleStorage.getFile('hyperiot-storage1', filename);
+  res.download(file);
+});
 module.exports = router;
