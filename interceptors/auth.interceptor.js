bdc55d45c502bc8328fc8e6f6ee87bdc5eeaef71
jonatan gall <jonatangd.souza@gmail.com>
2021-06-21 11:08:14 -0300

    mock interceptor do dev

--- interceptors/auth.interceptor.js
+++ interceptors/auth.interceptor.js
@@ -3,25 +3,28 @@ const participant = require('../models/participants.model')
 const ENV = require('../config/env');
 
 const auth = async(req, res, next) => {
-    const headerAuth = req.header('Authorization');
-    if(!headerAuth){
-        res.status(401).json({ error: 'Not Authorized' })
-        return;
-    }
-    const token = headerAuth.replace('Bearer ', '')
-    try {
-        const data = jwt.verify(token, ENV.JWT_KEY)
-        const result = await participant.participantExists(data.email);
-        if (result) {
-            req.sessionEmail = data.email;
-            next();
-            return;
-        }
-    } catch(e) {
-
-    }
-    res.status(401).json({ error: 'Not Authorized' })
+    req.sessionEmail = 'jhondoe@example.com';
+    next();
     return;
+    // const headerAuth = req.header('Authorization');
+    // if(!headerAuth){
+    //     res.status(401).json({ error: 'Not Authorized' })
+    //     return;
+    // }
+    // const token = headerAuth.replace('Bearer ', '')
+    // try {
+    //     const data = jwt.verify(token, ENV.JWT_KEY)
+    //     const result = await participant.participantExists(data.email);
+    //     if (result) {
+    //         req.sessionEmail = data.email;
+    //         next();
+    //         return;
+    //     }
+    // } catch(e) {
+
+    // }
+    // res.status(401).json({ error: 'Not Authorized' })
+    // return;
     
 
 }
